c7c300b54a3603b315cbf3b8b53bc28e81d5dfb2
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2021-08-24 11:10:50 +0100

    Update react.js

--- react.js
+++ react.js
@@ -7,3 +7,26 @@ let name = 'Earl'
 
 //third way to declare a vatriable that doest change
 const name ='Zeus'
+
+//next creating template strings
+let fname = 'Earl';
+let lname = 'Zeus';
+let age = promt("Gues Earl's age ...")
+
+let result = ~${fname} ${lname} 'is' ${age} 'years old.'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
