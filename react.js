e9145519b8da58b71732d330266df95614a2c821
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2021-08-24 11:04:16 +0100

    Update react.js

--- react.js
+++ react.js
@@ -1,2 +1,9 @@
- var name = 'Millen';
+//creaating variables in js in 3 ways
+//first way
+var name = 'Millen';
 atlert(name);
+//second way conditional variable
+let name = 'Earl'
+
+//third way to declare a vatriable that doest change
+const name ='Zeus'
